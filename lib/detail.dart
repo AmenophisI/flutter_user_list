05c8e48f054b517d1612cab65c3e8b52499aeafc
Ryosuke <r-moritani@pai-r.com>
2025-01-15 18:21:09 +0900

    詳細画面にもお気に入り機能を追加

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/link.dart';
 
-class Detail extends StatelessWidget {
+class Detail extends StatefulWidget {
   const Detail({super.key, required this.user});
 
   final dynamic user;
+
+  @override
+  State<Detail> createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  List<String> favorites = [];
+  bool isFavorite = false;
+
+  Future<void> getFavorites() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      favorites = prefs.getStringList('favorites') ?? [];
+      isFavorite = favorites.contains(widget.user['id'].toString());
+    });
+  }
+
+  Future<void> toggleFavorite() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      final userId = widget.user['id'].toString();
+      if (favorites.contains(userId)) {
+        favorites.remove(userId);
+      } else {
+        favorites.add(userId);
+      }
+      isFavorite = favorites.contains(userId);
+      prefs.setStringList('favorites', favorites);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getFavorites();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,20 +54,33 @@ class Detail extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.network(
-              user['avatar_url'],
+              widget.user['avatar_url'],
               height: 100,
               width: 100,
             ),
             Text(
-              user['login'],
+              widget.user['login'],
               style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
             ),
             Link(
-                uri: Uri.parse(user['html_url']),
-                builder: (context, followLink) {
-                  return TextButton(
-                      onPressed: followLink, child: Text(user['html_url']));
-                })
+              uri: Uri.parse(widget.user['html_url']),
+              builder: (context, followLink) {
+                return TextButton(
+                  onPressed: followLink,
+                  child: Text(widget.user['html_url']),
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            IconButton(
+              onPressed: toggleFavorite,
+              icon: Icon(
+                isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: isFavorite ? Colors.red : null,
+              ),
+              iconSize: 40,
+              tooltip: isFavorite ? "お気に入りに登録済み" : "お気に入りに登録",
+            ),
           ],
         ),
       ),
